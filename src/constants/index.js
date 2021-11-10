494991b3a9bfa820bdde3508e2759ef9a9f799fe
Cory <coryitpro@gmail.com>
2021-11-10 10:47:14 -0500

    Update the roadmap

--- src/constants/index.js
+++ src/constants/index.js
@@ -88,27 +88,23 @@ export const RoadmapSteps = [
   },
   {
     percentage: 40,
-    title: "DON’T STOP THE MUSIC",
-    content:
-      "Access to an Exclusive investments and business club (meetings, conferences, social events)… That’s not all, the lucky owners of our SPECIAL EDITION pieces will get monthly rewards and gifts as $500/month.",
+    title: "Coming soon",
+    content: "Coming soon",
   },
   {
     percentage: 60,
-    title: "CHAMPAGNE SHOWER",
-    content:
-      "In the Billionaire club, we are dedicated to giving back. For that reason, $30,000 will be given to a reputable charity picked by the community during a vote on Discord. Started from the bottom, we don’t forget where we come from.",
+    title: "Coming soon",
+    content: "Coming soon",
   },
   {
     percentage: 80,
-    title: "MAKE IT RAIN",
-    content:
-      "Daily raffles for an entire month with a chance to win $1000 per day. Monthly surprises for Party APE Holders : unlock exclusive merch release from the Billionaire Club’s Clockroam for holders. We will also create collectible figures and launch collaborations with artists. You will have to stay in touch for monthly Airdrops with future benefits.",
+    title: "Coming soon",
+    content: "Coming soon",
   },
   {
     percentage: 100,
-    title: "AFTER PARTY",
-    content:
-      "100% The Party Ape liquidity pool is initiated. It’s time to initiate the next steps. Holders spotting the chapter two clues will have access to early rewards giving them a major heads up.",
+    title: "Coming soon",
+    content: "Coming soon",
   },
 ]
 
